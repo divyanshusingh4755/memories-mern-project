539e042d89eb8b47b932321b8d6e784ed118b3be
Divyanshu <divyanshu@bluerickshaw.com>
2021-05-20 20:17:44 +0530

    JWT login

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -4,23 +4,39 @@ import { GoogleLogin } from "react-google-login"
 import { useDispatch } from "react-redux";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import { useHistory } from "react-router-dom";
+import { signin, signup } from "../../actions/auth"
 import useStyles from './styles';
 import Input from './Input'
 import Icon from './icon'
 
+
+const intialState = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    confirmPassword: ''
+}
 function Auth() {
     const classes = useStyles();
     const [showPassword, setShowPassword] = useState(false)
     const [isSignup, setIsSignup] = useState(false)
+    const [formData, setFormData] = useState(intialState)
     const dispatch = useDispatch();
     const history = useHistory();
 
-    const handleSubmit = () => {
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (isSignup) {
+            dispatch(signup(formData, history))
+        } else {
+            dispatch(signin(formData, history))
+        }
 
     }
 
-    const handleChange = () => {
-
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value })
     }
 
     const handleShowPassword = () => setShowPassword((prevShowPassword) => !prevShowPassword)
