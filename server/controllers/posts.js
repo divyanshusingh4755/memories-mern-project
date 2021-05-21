8b13732d91a93e541de0397c52c87e96765be177
Divyanshu <divyanshu@bluerickshaw.com>
2021-05-21 08:51:48 +0530

    Auth Middleware

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -38,9 +38,17 @@ export const deletePost = async (req, res) => {
 
 export const likePost = async (req, res) => {
     const { id } = req.params;
+    if (!req.userId) res.status(401).json({ message: "Unauthenticated" });
     if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send("No post with that id");
     const post = await PostMessage.findById(id);
-    const updatedPost = await PostMessage.findByIdAndUpdate(id, { likeCount: post.likeCount + 1 }, { new: true });
+    const index = post.likes.findIndex((id) => id === String(req.userId));
 
-    res.json(updatePost)
+    if (index === -1) {
+        post.likes.push(req.userId);
+    } else {
+        post.likes = post.likes.filter((id) => id !== String(req.userId));
+    }
+    const updatedPost = await PostMessage.findByIdAndUpdate(id, post, { new: true });
+
+    res.json(updatedPost)
 }
