07d6e94316263a1b0a5c306cf192738fdc18789b
Divyanshu <divyanshu@bluerickshaw.com>
2021-05-22 21:01:00 +0530

    Modified Add, delete and edit

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -41,9 +41,15 @@ export const deletePost = async (req, res) => {
 
 export const likePost = async (req, res) => {
     const { id } = req.params;
-    if (!req.userId) res.status(401).json({ message: "Unauthenticated" });
-    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send("No post with that id");
+
+    if (!req.userId) {
+        return res.json({ message: "Unauthenticated" });
+    }
+
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
+
     const post = await PostMessage.findById(id);
+
     const index = post.likes.findIndex((id) => id === String(req.userId));
 
     if (index === -1) {
@@ -52,8 +58,7 @@ export const likePost = async (req, res) => {
         post.likes = post.likes.filter((id) => id !== String(req.userId));
     }
     const updatedPost = await PostMessage.findByIdAndUpdate(id, post, { new: true });
-
-    res.json(updatedPost)
+    res.status(200).json(updatedPost);
 }
 
 export default router;
