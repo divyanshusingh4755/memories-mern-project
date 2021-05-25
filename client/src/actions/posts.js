b381628a1d5056c6e77b73eb5bc91de70a830242
Divyanshu <divyanshusingh4755@gmail.com>
2021-05-25 21:03:19 +0530

    Loading

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,11 +1,13 @@
 import * as api from '../api'
-import { FETCH_ALL, CREATE, UPDATE, DELETE, FETCH_BY_SEARCH } from "../constants/actionTypes"
+import { FETCH_ALL, CREATE, UPDATE, DELETE, FETCH_BY_SEARCH, START_LOADING, END_LOADING } from "../constants/actionTypes"
 
 // Action Creators
 export const getPosts = (page) => async (dispatch) => {
     try {
+        dispatch({ type: START_LOADING });
         const { data } = await api.fetchPosts(page);
-        dispatch({ type: FETCH_ALL, payload: data })
+        dispatch({ type: FETCH_ALL, payload: data });
+        dispatch({ type: END_LOADING });
     } catch (error) {
         console.log(error)
     }
@@ -13,8 +15,10 @@ export const getPosts = (page) => async (dispatch) => {
 
 export const getPostsBySearch = (searchQuery) => async (dispatch) => {
     try {
+        dispatch({ type: START_LOADING });
         const { data: { data } } = await api.fetchPostsBySearch(searchQuery);
-        dispatch({ type: FETCH_BY_SEARCH, payload: data })
+        dispatch({ type: FETCH_BY_SEARCH, payload: data });
+        dispatch({ type: END_LOADING });
     } catch (error) {
         console.log(error)
     }
@@ -22,8 +26,10 @@ export const getPostsBySearch = (searchQuery) => async (dispatch) => {
 
 export const createPost = (post) => async (dispatch) => {
     try {
+        dispatch({ type: START_LOADING });
         const { data } = await api.createPost(post);
-        dispatch({ type: CREATE, payload: data })
+        dispatch({ type: CREATE, payload: data });
+        dispatch({ type: END_LOADING });
     } catch (error) {
         console.log(error)
     }
@@ -32,7 +38,7 @@ export const createPost = (post) => async (dispatch) => {
 export const updatePost = (id, post) => async (dispatch) => {
     try {
         const { data } = await api.updatePost(id, post)
-        dispatch({ type: UPDATE, payload: data })
+        dispatch({ type: UPDATE, payload: data });
     } catch (error) {
         console.log(error)
     }
@@ -41,7 +47,7 @@ export const updatePost = (id, post) => async (dispatch) => {
 export const deletePost = (id) => async (dispatch) => {
     try {
         await api.deletePost(id);
-        dispatch({ type: DELETE, payload: id })
+        dispatch({ type: DELETE, payload: id });
     } catch (error) {
         console.log(error)
     }
@@ -50,7 +56,7 @@ export const deletePost = (id) => async (dispatch) => {
 export const likePost = (id) => async (dispatch) => {
     try {
         const { data } = await api.likePost(id)
-        dispatch({ type: UPDATE, payload: data })
+        dispatch({ type: UPDATE, payload: data });
     } catch (error) {
         console.log(console.error());
     }
--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -84,9 +84,11 @@ function Home() {
                             </Button>
                         </AppBar>
                         <Form currentId={currentId} setCurrentId={setCurrentId} />
-                        <Paper elevation={6}>
-                            <Paginate page={page} />
-                        </Paper>
+                        {(!searchQuery && !tags.length) && (
+                            <Paper elevation={6} className={classes.pagination}>
+                                <Paginate page={page} />
+                            </Paper>
+                        )}
                     </Grid>
                 </Grid>
             </Container>
